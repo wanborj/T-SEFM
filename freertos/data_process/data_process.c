e610c6644bcc0fcb91ad86789b5d8cefc7874704
wanbo <wanborj@mail.ustc.edu.cn>
2016-04-12 21:04:49 +0800

    no bug completed test-2.0

---
+++ freertos/data_process/data_process.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: data_proccess.c
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 12 Apr 2016 06:04:50 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    int id1, id2;
+    long long int start_time, end_time;
+    freopen("../executionTimeOfServant.data", "r", stdin);
+    freopen("data_process.data", "w", stdout);
+    
+    while( scanf("%d", &id1) != EOF )
+    {
+        scanf("%lld", &start_time);
+        scanf("%d", &id2);
+        scanf("%lld", &end_time);
+
+        if( id2 == (id1 + 10) * 3 )
+        {
+            printf("%d %lf\n", id1, (end_time - start_time)/1000000.0); // transit to micro second.
+        }
+        else
+        {
+            fprintf(stdout, "Servant %d had been preempted\n", id1);
+        }
+    }
+
+    fclose(stdin);
+    fclose(stdout);
+
+    return 0;
+}
